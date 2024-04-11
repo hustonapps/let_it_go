4e8d06cdc8df858055d3ff8b90ee9545d4a099d8
Matt Huston <hustonapps@gmail.com>
2024-04-11 13:38:54 -0600

    goroutines and channels

--- main.go
+++ main.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"fmt"
-	"let_it_go/cmdmanager"
-	// "let_it_go/filemanager"
+	// "let_it_go/cmdmanager"
+	"let_it_go/filemanager"
 	"let_it_go/prices"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	for _, taxRate := range taxRates {
-		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate * 100))
-		cmdm := cmdmanager.New()
-		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
-		err := priceJob.Process()
+	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
 
-		if err != nil {
-			fmt.Println("could not process job: ", err)
+	for index, taxRate := range taxRates {
+		doneChans[index] = make(chan bool)
+		errorChans[index] = make(chan error)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate * 100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		go priceJob.Process(doneChans[index], errorChans[index], index)
+	}
+
+	for index := range taxRates {
+		select {
+		case err := <- errorChans[index]:
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <- doneChans[index]:
+			fmt.Println(("done"))
 		}
 	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,6 +1,7 @@
 package prices
 
 import (
+	"errors"
 	"fmt"
 	"let_it_go/conversion"
 	"let_it_go/iomanager"
@@ -30,11 +31,16 @@ func (job *TaxIncludedPriceJob) LoadData() error {
 	return nil
 }
 
-func (job *TaxIncludedPriceJob) Process() error {
+func (job *TaxIncludedPriceJob) Process(doneChan chan bool, errorChan chan error, index int) {
 	err := job.LoadData()
 
+	if index == 2 {
+		err = errors.New("simulated failure")
+	}
+
 	if err != nil {
-		return err
+		errorChan <- err
+		return
 	}
 
 	result := make(map[string]string)
@@ -45,7 +51,8 @@ func (job *TaxIncludedPriceJob) Process() error {
 	}
 
 	job.TaxIncludedPrices = result
-	return job.IOManager.WriteResult(job)
+	job.IOManager.WriteResult(job)
+	doneChan <- true
 }
 
 func NewTaxIncludedPriceJob(fm iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
